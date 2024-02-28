f93a1b3152a1b294ba7d39635f3bd6711b88c770
vaaliferov <vaaliferov@gmail.com>
2024-02-28 15:36:37 +0300

    clean up

--- bot.py
+++ bot.py
@@ -1,27 +1,30 @@
-import math
-import subprocess
+import matplotlib
 import numpy as np
-import telegram.ext
 import matplotlib.pyplot as plt
-from secret import *
+import math, argparse, subprocess
+from telegram.ext import Updater, MessageHandler, Filters
 
-SR = 16000
-source, shadow = {}, {}
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('id', type=int, help='bot owner id')
+    parser.add_argument('token', type=str, help='bot token')
+    return parser.parse_args()
 
 def load_voice(context, file_id):
     context.bot.getFile(file_id).download('a.ogg')
-    args = ['opusdec', '--quiet', '--rate', str(SR), 'a.ogg', '-']
+    args = ['opusdec', '--quiet', '--rate', str(16000), 'a.ogg', '-']
     return np.frombuffer(subprocess.check_output(args), dtype=np.int16)
 
 def send_voice(context, chat_id, audio, caption=''):
     args = ['opusenc', '--quiet', '--raw-chan', '1', 
-            '--raw-rate', str(SR), '-', 'a.ogg']
+            '--raw-rate', str(16000), '-', 'a.ogg']
     subprocess.check_output(args, input=audio.tobytes())
     with open('a.ogg', 'rb') as fd:
         context.bot.send_voice(chat_id, fd, caption=caption)
 
 def split_audio(audio, n_secs=2):
-    n_pieces = math.ceil(len(audio) / (n_secs * SR))
+    n_pieces = math.ceil(len(audio) / (n_secs * 16000))
     return np.array_split(audio, n_pieces)
 
 def send_plot(context, chat_id, audio, caption=''):
@@ -32,20 +35,27 @@ def send_plot(context, chat_id, audio, caption=''):
 
 def send_spec(context, chat_id, audio, caption=''):
     fig, ax = plt.subplots(); fig.set_size_inches(6,3)
-    ax.specgram(audio, Fs=SR, NFFT=512)
+    ax.specgram(audio, Fs=16000, NFFT=512)
     fig.savefig('p.jpg'); plt.close(fig)
     with open('p.jpg', 'rb') as fd:
         context.bot.send_photo(chat_id, fd, caption=caption)
 
+
+args = parse_args()
+matplotlib.use('Agg')
+source, shadow = {}, {}
+
+
 def handle_voice(update, context):
+
     user = update.message.from_user
     file_id = update.message.voice['file_id']
     audio = load_voice(context, file_id)
-    
-    if user['id'] != TG_BOT_OWNER_ID:
+
+    if user['id'] != args.id:
         msg = f"@{user['username']} {user['id']}"
-        context.bot.send_message(TG_BOT_OWNER_ID, msg)
-        context.bot.send_voice(TG_BOT_OWNER_ID, file_id)
+        context.bot.send_message(args.id, msg)
+        context.bot.send_voice(args.id, file_id)
     
     if user['id'] not in source:
         source[user['id']] = split_audio(audio[::-1])
@@ -67,8 +77,9 @@ def handle_voice(update, context):
         send_voice(context, user['id'], audio, 'done!')
         del source[user['id']], shadow[user['id']]
 
-h = telegram.ext.MessageHandler
-vf = telegram.ext.Filters.voice 
-u = telegram.ext.Updater(TG_BOT_TOKEN)
-u.dispatcher.add_handler(h(vf, handle_voice))
-u.start_polling(); u.idle()
+
+updater = Updater(args.token)
+voice_handler = MessageHandler(Filters.voice, handle_voice)
+updater.dispatcher.add_handler(voice_handler)
+updater.start_polling()
+updater.idle()
--- readme.md
+++ readme.md
@@ -1,5 +1,13 @@
-#### A Funny Game Based on Reversing Audio
+#### SSA Game: Reversing Audio
 
 This is an implementation of a game called "SSA". It was invented by Aleksandr Pushnoy back in 2004. The idea is simple - you're singing a song, it's being recorded, then reversed and cut in pieces. Someone from the opposite team then listens to it and tries to imitate the same with their own voice. Finally, it's reversed again, and the task is to guess what song it was :) [[demo](https://t.me/vaaliferov_ssa_bot)]
+
+```bash
+python3 -m venv env
+source env/bin/activate
+apt install -y opus-tools
+pip install -r requirements.txt
+python3 bot.py <bot_owner_id> <bot_token>
+```
 
 ![Alt Text](pics/tg.png)  
